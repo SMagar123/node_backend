b1f055cc7378f72ff44022199077020ecfa8605a
SMagar123 <sooraazmagar@gmail.com>
2024-03-19 02:12:07 +0545

    add(register-api): add basic api for registering the user and saving it in mongodb collection

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,15 +17,13 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const { fullName, email, username, password } = req.body;
 
-  console.log("fullName:", fullName, email);
-
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new APIError(400, "All fields are required !");
   }
   //checks the user with username or email in mongodb collection
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -34,7 +32,16 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new APIError(400, "Avatar file is required !");
