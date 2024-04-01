d181807ebecd78f6bb7b2cee3eac9894789ff56c
SMagar123 <sooraazmagar@gmail.com>
2024-04-01 23:19:46 +0545

    add(aggregation): use mongodb aggregation pipeline for fetching the join data

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -21,6 +21,77 @@ const generateAccessAndRefreshTokens = async (userId) => {
   }
 };
 
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  if (!username?.trim()) {
+    throw new APIError(400, "Username is missing");
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        rom: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
+  ]);
+
+  if (!channel?.length) {
+    throw new APIError(404, "Channel doesnot exist");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new APIResponse(200, channel?.[0], "User channel fetched successfully")
+    );
+});
+
 const registerUser = asyncHandler(async (req, res) => {
   //get user details from frontend
   //validation - not empty
@@ -246,7 +317,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
   if (!fullName || !email) {
     throw new APIError(400, "All fields are required !");
   }
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
@@ -272,6 +343,7 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
   if (!avatar.url) {
     throw new APIError(400, "Error while uploading on avatar !");
   }
+  //TODO :- DELETE THE OLD AVATAR AFTER THE NEW UPLOAD
 
   const user = await User.findByIdAndUpdate(
     req.user?._id,
@@ -324,4 +396,5 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
 };
--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ connectDB()
       console.log("err::", error);
       throw error;
     });
-
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running at port ${process.env.PORT}`);
     });
